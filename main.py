71885ee6b649ce0fc27ae527664e6b029a5596c5
Paulo Véliz <pvelizs16@gmail.com>
2022-12-09 23:18:42 -0500

    Lista pokemones por forma

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[39m'
+
+#Variables fijas de estilo
+BRIGHT = '\033[1m'
+DIM = '\033[2m'
+NORMAL = '\033[22m'
+RESET_ALL = '\033[0m'
+
 import requests
 
-if __name__ == '__main__':
-    url = 'https://pokeapi.co/api/v2/pokemon-form/'
+def get_pokemons(url='https://pokeapi.co/api/v2/pokemon-form/', offset=0):
+    args = {'offset' : offset} if offset else {}
 
-    response = requests.get(url)
+    response = requests.get(url, params = args)
+
     if response.status_code == 200:
 
         payload = response.json()
@@ -13,3 +30,11 @@ if __name__ == '__main__':
             for pokemon in results:
                 name = pokemon['name']
                 print(name)
+        
+        next = input(BRIGHT+CYAN+'\n¿DESEA CONTINUAR LISTA? Y/N'+RESET_ALL).lower()
+        if next == 'y':
+            get_pokemons(offset=offset+20)
+
+if __name__ == '__main__':
+    url = 'https://pokeapi.co/api/v2/pokemon-form/'
+    get_pokemons()
